a6d9afa4e1b58e7943a25cfc0a85588e26eac2bd
Alan Altamirano Hernandez <altamiranohernandezalan@gmail.com>
2025-01-20 18:58:46 -0600

    Manejo primeras vistas

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -2,12 +2,23 @@
 
 //Funciones para el controlador usuario
 const formularioLogion = (req, res) =>{
-    res.render("auth/login");
+    res.render("auth/login",{
+        pagina: "Iniciar Sesión"
+    });
 }
 const formularioRegistro = (req, res) =>{
-    res.render("auth/registro")
+    res.render("auth/registro", {
+        pagina: 'Crear Cuenta'
+    })
 }
+const olvidePassword = (req, res) =>{
+    res.render("auth/olvide-password", {
+        pagina: "Recupera tu Password"
+    })
+}
+
 export {
     formularioLogion,
-    formularioRegistro
+    formularioRegistro,
+    olvidePassword
 }
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,11 +1,12 @@
 import express  from "express";
 import {
-    formularioLogion, formularioRegistro
+    formularioLogion, formularioRegistro, olvidePassword
 } from "../controllers/usuarioController.js";
 const router = express.Router();
 
 //Rutas para el usuario
 router.get("/login", formularioLogion);
 router.get("/registro", formularioRegistro);
+router.get("/olvide-password", olvidePassword);
 
 export default router;
