b476988b5190dc60658d16d8a42777e444147406
Alan Altamirano Hernandez <altamiranohernandezalan@gmail.com>
2025-03-02 21:12:14 -0600

    Editando detalles de propiedades en el mapa de area publica

--- src/js/mapaInicio.js
+++ src/js/mapaInicio.js
@@ -23,10 +23,18 @@
         const {propiedades} = data;
         propiedades.forEach((propiedad, index) =>{
             //Agrgeamos los pines al mapa
-            const maker =  new L.marker([propiedad.lat, propiedad.lng], {
+            const maker =  new L.Marker([propiedad.lat, propiedad.lng], {
                 draggable: true,
                 autoPan: true
-            }).addTo(mapa).bindPopup(propiedad.calle)
+            }).addTo(mapa).bindPopup(`
+                <p class="font-bold text-indigo-600 text-sm uppercase font-bold">${propiedad.categoria.nombre}</span> </p>
+                <h1 class="text-xl font-extrabold uppercase text-center my-5">${propiedad.titulo}</h1>
+                <img src="/uploads/${propiedad.imagen}">
+                <p class="font-bold text-xl">Precio: <span class="font-normal">${propiedad.precio.nombre}</span></p>
+                <p class="font-bold text-xl">Direccion: <span class="font-normal">${propiedad.calle}</span></p>
+                <a href="/propiedad/${propiedad.id}" class="font-bold text-xl text-center bg-green-600 rounded px-3 py-2 block uppercase">Detalles</a>
+            `)
+            //Esta parte del codigo debe revisarla para que funcione el filtro de busqueda Video 149
             // makers.addLayer(maker);
 
         });
