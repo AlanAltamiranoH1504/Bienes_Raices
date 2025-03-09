be2c5ffa08ddb102bae02e9b42aa4a6c4e6226ca
Alan Altamirano Hernandez <altamiranohernandezalan@gmail.com>
2025-03-08 18:30:27 -0600

    feat: add status notifications for sending messages

--- controllers/mensajeController.js
+++ controllers/mensajeController.js
@@ -1,17 +1,71 @@
-import {Mensaje} from "../models/index.js";
+import {Categoria, Mensaje, Precio} from "../models/index.js";
+import Propiedad from "../models/Propiedad.js";
+import jwt from "jsonwebtoken";
+import Usuario from "../models/Usuario.js";
+import esVendedor from "../helpers/EsVendedor.js";
 
-const envioMensaje = async (req, res) =>{
+const envioMensaje = async (req, res) => {
     const requestBody = req.body;
     const {usuario_envia, mensaje, propiedad_id} = requestBody;
+    const cookie = req.cookies.token;
+    let iniciarSesion;
+    let usuario;
+    let mostrarForm;
 
-    try{
+    const propiedad = await Propiedad.findOne({
+        where: {id: propiedad_id}, include: [
+            {model: Categoria, attributes: ['id', 'nombre']},
+            {model: Precio, attributes: ['id', 'nombre']}
+        ]
+    });
+    if(!cookie){
+        console.log("No existe la cookie con el token. Debe iniciar sesion");
+        iniciarSesion = true;
+    }else{
+        console.log("Existe la cookie en el token")
+        const token = jwt.verify(cookie, process.env.JWT_SECRET);
+        if (!token.id){
+            iniciarSesion = true;
+        }else {
+            const id = token.id;
+            usuario = await Usuario.findByPk(id);
+            iniciarSesion = false;
+            //Verificamos si el usuario es dueño de la propiedad
+            mostrarForm = esVendedor(usuario, propiedad);
+        }
+    }
+
+    if (mensaje.trim() === "" || mensaje === null || mensaje.length <= 10) {
+        res.render("../views/propiedades/mostrar", {
+            propiedad,
+            barra: true,
+            pagina: propiedad.titulo,
+            iniciarSesion,
+            mostrarForm,
+            usuario,
+            csrf: req.csrfToken(),
+            msg_error: true
+        });
+        return;
+    }
+    try {
         const mensajeCreado = await Mensaje.create({mensaje, usuario_id: usuario_envia, propiedad_id});
-        if (mensajeCreado){
-            res.redirect(`/propiedad/${propiedad_id}`);
-        }else{
+        if (mensajeCreado) {
+            res.render("../views/propiedades/mostrar", {
+                propiedad,
+                barra: true,
+                pagina: propiedad.titulo,
+                iniciarSesion,
+                mostrarForm,
+                usuario,
+                csrf: req.csrfToken(),
+                msg: true
+            });
+            // res.redirect(`/propiedad/${propiedad_id}`);
+        } else {
             console.log("NO SE CREO EL MENSAJE EN LA DB");
         }
-    }catch (e){
+    } catch (e) {
         console.log("ERROR: " + e)
     }
 }
